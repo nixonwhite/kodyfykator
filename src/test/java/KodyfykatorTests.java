23d2cb52259965463925501619f55f50367a875b
nickkon <nixon.white@gmail.com>
2022-04-25 12:50:44 +0300

    Refactoring.

--- src/test/java/KodyfykatorTests.java
+++ src/test/java/KodyfykatorTests.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
