ba7c9ef6ba5cd25138262f01ebb19cd3cc3ed7dc
nickkon <nixon.white@gmail.com>
2024-07-22 14:19:24 +0300

    SB + deps update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.7"
-    id("io.spring.dependency-management") version "1.1.5"
-    id("org.sonarqube") version "5.0.0.4638"
+    id("org.springframework.boot") version "3.2.8"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.sonarqube") version "5.1.0.4882"
     id("io.snyk.gradle.plugin.snykplugin") version "0.6.1"
 }
 
 group = "ua.org.gurt"
-version = "1.1.22"
+version = "1.1.23"
 description = "kodyfykator"
 
-val jacksonCoreVersion = "2.17.1"
+val jacksonCoreVersion = "2.17.2"
 val jsonVersion = "20240303"
 
 repositories {
