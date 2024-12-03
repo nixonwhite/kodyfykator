bed55b1745369ad467dd182d14ae43f0e331243c
nickkon <nixon.white@gmail.com>
2024-12-03 14:29:42 +0200

    Circular view path fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,6 @@ configurations {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("com.fasterxml.jackson.core:jackson-core:${jacksonCoreVersion}")
     implementation("org.json:json:${jsonVersion}")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
