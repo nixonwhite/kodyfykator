16d97bd0709be8ee91d138eaa143136e62dbfa3e
nickkon <nixon.white@gmail.com>
2024-12-18 17:55:32 +0200

    Deps update; references update;

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     java
     id("org.springframework.boot") version "3.4.0"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "ua.org.gurt"
-version = "1.1.31"
+version = "1.1.32"
 description = "kodyfykator"
 
 val jacksonCoreVersion = "2.18.2"
