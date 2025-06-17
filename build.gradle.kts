219f7eadb2ed7d5f128f0855dc5c72fb11748260
nickkon <nixon.white@gmail.com>
2025-06-17 11:55:13 +0300

    SB, deps update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.5"
+    id("org.springframework.boot") version "3.4.6"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "6.1.0.5360"
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "ua.org.gurt"
-version = "1.1.41"
+version = "1.1.42"
 description = "kodyfykator"
 
 val jacksonCoreVersion = "2.19.0"
-val jsonVersion = "20250107"
+val jsonVersion = "20250517"
 
 repositories {
     mavenCentral()
